123aab92c5b0fa19f7e693df52c592e1424dec51
tools4git <>
2021-05-05 19:35:13 +0800

    更新4000+短视频

--- video/check_url_exists.sh
+++ video/check_url_exists.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #cat /dev/null >a.txt
-cat $1 | while read line
-do
-  type=$(curl -s --head $line |grep 'video/mp4')
+line=$1
+#type=$(curl -s --head $line |grep 'video/mp4')
+type=$(curl -s -o /dev/null -I -w "%{content_type}" $line)
+if [ -n "$type" ]; then
+  echo $line>>a.txt
+else
+  echo "不存在：${line##*.net}" 
+  newline=$(echo $line|sed 's/ali-cdn.kwai.net/yximgs.com/g')
+  type=$(curl -s -o /dev/null -I -w "%{content_type}" $newline)
   if [ -n "$type" ]; then
-    echo $line>>a.txt
+    echo $newline>>a.txt
   else
-    echo "不存在：${line##*.net}" 
-    newline=$(grep ${line##*.net} $1.bak)
-    if [ -n "$newline" ]; then
-      echo $newline>>a.txt
-    else
-      echo $line>>NOTFOUND.txt
-    fi
+    echo $line>>NOTFOUND.txt
   fi
-done
+fi
